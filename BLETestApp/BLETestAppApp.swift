11ddb8c39902d0f1f912a01a82a26ccc996f7d38
Konstantin Timoshenko <rondo.devil@gmail.com>
2023-05-06 20:57:24 +0300

    Initial Commit

---
+++ BLETestApp/BLETestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLETestAppApp.swift
+//  BLETestApp
+//
+//  Created by Константин Тимошенко on 06.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct BLETestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
